1c9cb442c47312254ef11a64d7de6ab5f3c9ceff
Deeksha Chelamalla <112186227+DeekshaChelamalla25@users.noreply.github.com>
2023-01-17 09:06:19 -0600

    Add files via upload

---
+++ MyPlayground1.playground/Contents.swift
@@ -0,0 +1,33 @@
+import Cocoa
+
+var greeting = "Hello, Doctor"
+var i = 10;
+print(greeting)
+// we use var(changes) and let(constant) to declare
+print("This is","Deeksha", 22)
+var name = "Chitti"
+print("Hello \(name)!")
+var petName = "Snoppy"
+var age = 5
+print("My pet name is \(petName) and it's age is \(age)")
+print("""
+Address:
+912 N Walnut St
+Apt #17
+Maryville MO
+64468
+""")
+print("It can walk.But,\rCan't see")
+let wekcomeString : String = "Hello!"
+print(wekcomeString, "All")
+print("Welcome to Swift Programming")
+print("Fall 2021")
+print("*************")
+print("Welcome to Swift Programming" , terminator : "-" )
+print("Fall 2021")
+print("The list of numbers are ")
+print(1,2,3,4,5,6)
+print("The new pattern is")
+print(1,2,3,4,5,6, separator: ",")
+
+
